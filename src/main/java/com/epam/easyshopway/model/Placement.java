650817f47c2b3e23cdbaf9c1b6dc11ef89c34854
Marko Tsura <markotsura@gmail.com>
2016-09-05 02:41:32 +0300

    Updated models v2

--- src/main/java/com/epam/easyshopway/model/Placement.java
+++ src/main/java/com/epam/easyshopway/model/Placement.java
@@ -9,6 +9,42 @@ public class Placement {
 	@Column("map_id")
 	private Integer mapId;
 
-	@Column("place")
-	private Integer place;
+	@Column("value")
+	private Integer value;
+
+	public Placement(Integer id, Integer mapId, Integer value) {
+		super();
+		this.id = id;
+		this.mapId = mapId;
+		this.value = value;
+	}
+
+	public Placement() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getMapId() {
+		return mapId;
+	}
+
+	public void setMapId(Integer mapId) {
+		this.mapId = mapId;
+	}
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+	
 }
