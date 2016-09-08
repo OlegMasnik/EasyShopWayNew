d0530d7d582f2fa1855402615107e2882a6506b1
Roman Frankiv <roma.fran95@gmail.com>
2016-09-08 21:55:20 +0300

    Fix cabinets, search and user history

--- src/main/java/com/epam/easyshopway/service/FullProductListService.java
+++ src/main/java/com/epam/easyshopway/service/FullProductListService.java
@@ -1,6 +1,8 @@
 package com.epam.easyshopway.service;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import com.epam.easyshopway.dto.FullProductListDTO;
@@ -21,5 +23,25 @@ public class FullProductListService {
 		} finally {
 			return fullProductLists;
 		}
+	}
+	
+	public static HashMap<Integer, List<FullProductList>> getGroupedList (Integer id) { 
+	    List<FullProductList> userProducts = new ArrayList<>(); 
+	    HashMap<Integer, List<FullProductList>> productLists = new HashMap<>(); 
+	     
+	    userProducts = getProductListByUserId(id); 
+	     
+	    List<FullProductList> current; 
+	    for (FullProductList product : userProducts) { 
+	      if ((current = productLists.get(product.getId())) == null) { 
+	        current = new ArrayList<>(); 
+	        current.add(product); 
+	        productLists.put(product.getId(), current); 
+	      } else { 
+	        current.add(product); 
+	      } 
+	    } 
+	     
+	    return productLists; 
 	}
 }
